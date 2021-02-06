ccf46c5477233d585d4a436e3a7749e916a63543
scottapp <scottapp>
2021-02-06 15:02:25 +0800

    parse float data from downloaded data

--- src/main_parse_ticker_info.py
+++ src/main_parse_ticker_info.py
@@ -1,13 +1,19 @@
 from yfin_downloader.utils import load_data
-import datetime
 from datetime import datetime as dt
 import os
 import json
-import codecs
+
+
+"""
+class Details(object):
+    def __init__(self):
+        self.outstandingShares = None
+        self.publicFloat = None
+"""
 
 
 def parse_ticker_info():
-    lines = load_data("data/OTCBB.txt")
+    lines = load_data("data/OTCBB-2021-01-29.txt")
     tickers = []
     for line in lines[1:]:
         cols = line.strip().split("\t")
@@ -15,27 +21,61 @@ def parse_ticker_info():
         tickers.append(ticker)
 
     working_dir = os.getcwd()
-    data_dir = '%s/data/info/2021-01-16' % working_dir
+    data_dir = '%s/data/info/2021-01-31' % working_dir
 
-    float_shares = dict()
+    infos = dict()
+    errors = dict()
     for ticker in tickers:
         try:
+            file_path = '%s/%s.json' % (data_dir, ticker)
+            if not os.path.isfile(file_path):
+                errors[ticker] = 'missing data file'
+                continue
+
             print(ticker)
-            file_path = '%s/%s_info.json' % (data_dir, ticker)
             with open(file_path, 'r', encoding='utf8') as f:
-                ticker_data = json.loads(f.read())
-                float_shares[ticker] = ticker_data['floatShares']
+                details = dict()
+                data = json.loads(f.read())
+                security_data = data.get('securities', [])
+                assert security_data, 'missing security data'
+                security_data = security_data[0]
+
+                details['publicFloat'] = security_data.get('publicFloat', None)
+                details['unrestrictedShares'] = security_data.get('unrestrictedShares', None)
+                details['publicFloatAsOfDate'] = security_data.get('publicFloatAsOfDate', 0)
+                details['unrestrictedSharesAsOfDate'] = security_data.get('unrestrictedSharesAsOfDate', 0)
+                infos[ticker] = details
+
         except Exception as ex:
+            print(ticker)
             print(ex)
+            errors[ticker] = str(ex)
             continue
 
-    with open('float_data.json', 'w', encoding='utf8') as f:
-        f.write(json.dumps(float_shares, indent=2, ensure_ascii=False))
+    today_date = dt.today().date()
+    with open('float_data_%s.json' % today_date, 'w', encoding='utf8') as f:
+        f.write(json.dumps(infos, indent=2, ensure_ascii=False))
+    print(errors)
+
+
+def purge_empty_file(dir_path):
+    files_to_delete = []
+    for file in os.listdir(dir_path):
+        if file.endswith('.json'):
+            statinfo = os.stat('%s/%s' % (dir_path, file))
+            if statinfo.st_size == 0:
+                print('%s, %s' % (file, statinfo.st_size))
+                files_to_delete.append(file)
+    print(len(files_to_delete))
+    for file in files_to_delete:
+        os.remove('%s/%s' % (dir_path, file))
 
 
 if __name__ == "__main__":
 
     try:
+        #dir_path = 'data/info/2021-01-31'
+        #purge_empty_file(dir_path)
         parse_ticker_info()
     except Exception as ex:
         print(ex)
